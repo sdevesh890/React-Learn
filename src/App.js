efe61b9161c669f32cb399224a3f06c34a1d44bb
sdevesh890 <74634378+sdevesh890@users.noreply.github.com>
2023-04-07 21:48:17 +0530

    added class component method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Func from './Components/Func';
+import ClassCompo from './Components/ClassCompo';
 function App()
 {
    return(
        <div>
         <h1>Hello React-learn</h1>
         <Func/>
+        <ClassCompo/>
        </div>
    );
 }
---
+++ src/Components/ClassCompo.js
@@ -0,0 +1,15 @@
+import React , {Component} from 'react';
+
+class ClassCompo extends Component
+{
+    render()
+    {
+        return(
+            <div>
+                <h1>This is a class Components</h1>
+            </div>
+        );
+    }
+}
+
+export default ClassCompo;
