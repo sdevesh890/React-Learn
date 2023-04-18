51bc1b448332285a361a9372985b44dda7dc7723
sdevesh890 <74634378+sdevesh890@users.noreply.github.com>
2023-04-18 06:37:54 +0530

    useEffect feature

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import './App.css';
-import Constructor from './LCM/Constructor';
-import Render from './LCM/Render';
-import ComponentDidmount from './LCM/ComponentDidmount';
-import Componentdidupdate from './LCM/Componentdidupdate';
-import Componentshoulupdate from './LCM/Componentshouldupdate';
+import UseEffec from './Hooks/UseEffec';
 function App()
 {
     
    return(
        <div>
-        <h1>Hello React-learn</h1>
-        {/* <Func/>
-        <ClassCompo/> */}
-        {/* <Propsprac name="Devesh Sharma" email="sdevesh890@gmail.com"/> */}
-        {/* <Stateprac/> */}
-        {/* <Getinput/> */}
-        {/* <Handleform/> */}
-        {/* <Validform/> */}
-        {/* <Constructor/> */}
-        {/* <Render/> */}
-        {/* <ComponentDidmount/> */}
-        {/* <Componentdidupdate/> */}
-        <Componentshoulupdate/>
+       <h1>Rect Home</h1>
+       <UseEffec/>
        </div>
 
    );
---
+++ src/Hooks/UseEffec.js
@@ -0,0 +1,14 @@
+import React,{useEffect}from 'react';
+function UseEffec()
+{
+    useEffect(()=>
+    {
+        alert("useEffect is working");
+    });
+    return(
+        <div>
+            <h1>okay</h1>
+        </div>
+    );
+}
+export default UseEffec;
